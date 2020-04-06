598d5cab5b5c881c4708ce252fe455a9a7f06e21
Seongho Jin <seongho4996@naver.com>
2020-04-06 23:46:03 +0900

    ps

---
+++ Codeforces/Round #631(div. 2)/B. Dreamoon Likes Permutations.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+const int SIZE = 200001;
+int arr[SIZE];
+int main(){
+
+    cin.tie(0);
+    ios_base :: sync_with_stdio(0);
+
+    int T;
+    cin >> T;
+    while(T--){
+        int n;
+        cin >> n;
+        int ma = 0;
+        
+        for(int i = 0; i < n; i++){
+            int t;
+        }
+    }
+}
