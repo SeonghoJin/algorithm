68e4ae5eb758ba5cad9269b9c867e21b5bacfa9b
Seongho Jin <seongho4996@naver.com>
2020-04-21 16:24:55 +0900

    ps

--- Javascript/test.js
+++ Javascript/test.js
@@ -1,5 +1,27 @@
-hoisted();
+hoisted(); // logs "foo" 
 
-function hoisted(){
-    console.log("hi");
+function hoisted() { console.log("foo"); }
+
+var notHoisted = function() { console.log("bar"); };
+
+var x = function(y) { return y * y; };
+
+(function () { // statements 
+})()
+
+var t= function(){
+
 }
+
+var tt = function ss(){
+
+}
+
+function aa(){
+
+}
+(function kk(){
+    
+}
+
+)
