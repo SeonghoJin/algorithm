66ead827f2273a8d2afd1d8ac2c53df0cea12230
Seongho Jin <seongho4996@naver.com>
2020-04-24 14:56:13 +0900

    MST-kruskal

---
+++ C++/BOJ/6497.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAX = 2*10e5+1;
+int N, M;
+int parent[MAX];
+
+int find(int i){return i == parent[i] ? i : parent[i] = find(parent[i]);}
+
+bool Union(int i, int j){
+	int a = find(i);
+	int b = find(j);
+	if(a == b)return false;
+	parent[a] = b;
+	return true;
+}
+
+int main(){
+	cin.tie(0);
+	ios_base :: sync_with_stdio(0);
+
+	while(1){
+	cin >> N >> M;
+	if(N == 0 && M == 0)break;
+	int ans = 0;
+	
+	priority_queue<pair<int,pair<int,int>>> pq;
+	for(int i = 0; i < N; i++)parent[i] = i;
+	for(int i = 0; i < M; i++){
+		int a, b, c;
+		cin >> a >> b >> c;
+		pq.push({-c,{a,b}});
+		ans+=c;
+	}
+	int K = 0;
+	
+	while(K != N-1){
+		
+		pair<int,pair<int,int>> temp = pq.top(); pq.pop();
+
+		int start = temp.second.first;
+		int end = temp.second.second;
+		int cost = temp.first;
+		
+		if(Union(start,end)){
+			ans+=cost;
+			K++;
+		}
+	}
+
+	cout << ans << '\n';
+	}
+}
+	
+
+
