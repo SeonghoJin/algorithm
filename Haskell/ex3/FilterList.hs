01585aa9a05a8f1b6bc82313c29dc92deffd43a3
SeonghoJin <seongho4996@naver.com>
2020-08-09 19:13:51 +0900

    study Maybe type and Prelude

--- Haskell/ex3/FilterList.hs
+++ Haskell/ex3/FilterList.hs
@@ -4,7 +4,26 @@ module FilterList where
 -- FilterList.hs
 data List t = E | C t (List t)
 
+filterList :: (t -> Bool) -> List t -> List t
 filterList _ E = E
 filterList p (C x xs)
   | p x       = C x (filterList p xs)
-  | otherwise = filterList p xs
+  | True      = filterList p xs
+  
+
+mapList :: (a -> b) -> List a -> List b
+mapList _ E = E
+mapList f (C x xs) = C (f x) (mapList f xs)
+
+safeHead :: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead (x : xs) = Just x
+
+data NonEmptyList a = NEL a [a]
+
+nelToList :: NonEmptyList a -> [a]
+nelToList (NEL x xs) = x : xs
+
+listToNel :: [a] -> Maybe (NonEmptyList a)
+listToNel [] = Nothing
+listToNel (x : xs) = Just $ NEL x xs
