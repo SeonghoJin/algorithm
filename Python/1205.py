38ccd83cb1c28d9de3fea5582aae3abdda385710
SeonghoJin <seongho4996@naver.com>
2020-09-23 14:27:29 +0900

    예외 케이스에 대해서 잘 생각해보자

--- Python/1205.py
+++ Python/1205.py
@@ -1,11 +1,25 @@
+import sys;
 m = dict()
+n,v,p = map(int, input().split(' '))
+if(n == 0):
+    print(1)
+    sys.exit()
+arr = map(int, input().split(' '))
 
-li = list([10,20,30,40])
+for value in arr :
+    m.setdefault(value,0)
+    m[value] += 1
 
+m.setdefault(v,0)
+m[v] += 1
+s = 0
 
-m.setdefault(10,0)
-m[10]+=1
-print(m[10])
-m.setdefault(10,0)
-m[10]+=1
-print(m[10])
+for key in sorted(m.keys(),reverse=True) :
+    s += m[key]
+    if(key == v):
+        break
+
+if s <= p :
+    print (s - m[v] + 1)
+else :
+    print(-1)
