4ada01a5da70e57d3b2c381b5d1e599c00cdc6d0
SeonghoJin <seongho4996@naver.com>
2021-02-17 19:05:49 +0900

    String, Date 문제 풀이

---
+++ SQL/루시와 엘라 찾기.sql
@@ -0,0 +1,12 @@
+-- 코드를 입력하세요
+SELECT
+ANIMAL_ID,
+NAME,
+SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME LIKE "Ella" or 
+NAME LIKE "Lucy" or
+NAME LIKE "Pickle" or
+NAME LIKE "Rogan" or
+NAME LIKE "Sabrina" or
+NAME LIKE "Mitty"
