1ca84e95168d7b234b4d0e265e707bb162b7f384
moretall <seongho4996@naver.com>
2022-12-12 21:19:31 +0900

    clean

---
+++ Javascript/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+        // if(Math.floor(num % i) !== 0)break;
+        // const t = parseInt(num / i);
+        // if(i === t){
+        //     ret += t;
+        // } else {
+        //     ret += (i + t);
+        // }
---
+++ Javascript/test.js
@@ -0,0 +1,7 @@
+const array = [1,2,3,4,5];
+
+const test = Array.of(...array);
+console.log(test);
+const test2 = Array.from(array)
+
+console.log(test2);
