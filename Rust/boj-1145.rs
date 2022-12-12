b96fbf08a9f4a7f7cb5cd8c142c5dc99a144e86d
moretall <seongho4996@naver.com>
2022-12-12 23:51:36 +0900

    solve boj 1145

--- Rust/boj-1145.rs
+++ Rust/boj-1145.rs
@@ -1,5 +1,25 @@
 use std::io;
+use std::cmp;
 
 fn main(){
-    print!("Hello World");
+    let mut line = String::new();
+
+    std::io::stdin().read_line(&mut line);
+
+    let numbers: Vec<i32>  = line.split_whitespace().map(|x| x.parse().unwrap()).collect();
+
+    for value in 1..1000000 {
+        let mut sum = 0;
+
+        for number in &numbers {
+            if value % number == 0 {
+                sum+=1;
+            }
+        }
+
+        if sum >= 3 {
+            println!("{}", value);
+            break;
+        }
+    }
 }
