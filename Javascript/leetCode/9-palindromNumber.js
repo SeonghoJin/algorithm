f409af47740f5d062e40896704c509e9271cfec2
seongho <seongho4996@naver.com>
2025-08-19 21:57:11 +0900

    9-palindromNumber.js 파일 추가: 주어진 숫자가 회문인지 확인하는 함수 구현

---
+++ Javascript/leetCode/9-palindromNumber.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+  if (x < 0) {
+    return false;
+  }
+
+  const str = x.toString();
+  let answer = false;
+  for (let i = 0; i <= str.length / 2; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+};
