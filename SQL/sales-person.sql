1917494dc751ebef510f199a2ad7307bca441fef
seongho <seongho4996@naver.com>
2025-08-31 12:58:58 +0900

    sales-person.sql 파일 수정: 주석 처리된 쿼리 제거 및 가독성 향상

--- SQL/sales-person.sql
+++ SQL/sales-person.sql
@@ -1,4 +1,19 @@
-Select name from SalesPerson as s 
-LEFT JOIN (Select o.sales_id from Orders as o
-LEFT join Company as c on c.com_id = o.com_id Where c.name = 'RED') as r on s.sales_id = r.sales_id
-Where r.sales_id is null
+-- Select name from SalesPerson as s 
+-- LEFT JOIN (Select o.sales_id from Orders as o
+-- LEFT join Company as c on c.com_id = o.com_id Where c.name = 'RED') as r on s.sales_id = r.sales_id
+-- Where r.sales_id is null
+
+SELECT
+    s.name
+FROM
+    salesperson s
+WHERE
+    s.sales_id NOT IN (SELECT
+            o.sales_id
+        FROM
+            orders o
+                LEFT JOIN
+            company c ON o.com_id = c.com_id
+        WHERE
+            c.name = 'RED')
+;
