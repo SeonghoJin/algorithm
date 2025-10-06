a07f55b355d0a331ba7a2ec88d3bf3395da879bd
seongho <seongho4996@naver.com>
2025-10-06 19:19:16 +0900

    feat: add commented-out SQL query for calculating cumulative games played

--- SQL/game-play-analysis-iii.sql
+++ SQL/game-play-analysis-iii.sql
@@ -1,3 +1,15 @@
 Select a.player_id, a.event_date, SUM(a2.games_played) as games_played_so_far from Activity a
 left join Activity a2 on a2.player_id = a.player_id and a.event_date >= a2.event_date
 group by player_id, event_date
+
+-- SELECT
+--   A.player_id,
+--   A.event_date,
+--   SUM(A.games_played) OVER (
+--     PARTITION BY
+--       A.player_id
+--     ORDER BY
+--       A.event_date
+--   ) AS games_played_so_far
+-- FROM
+--   Activity A;
