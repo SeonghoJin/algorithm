4bb7117fd1c680b62510e825f46ceb9ddf72553d
seongho <seongho4996@naver.com>
2025-10-07 20:40:55 +0900

    feat: add commented-out SQL queries for session duration analysis

--- SQL/create-a-session-bar-chart.sql
+++ SQL/create-a-session-bar-chart.sql
@@ -11,3 +11,16 @@ Select "15 or more" as bin, 900 as t1, 10000000000 as t2
 select b.bin, COUNT(s.session_id) as total from bins b
 left join Sessions s on s.duration >= b.t1 and s.duration < b.t2
 group by b.bin
+
+
+-- SELECT '[0-5>' AS 'bin', SUM(duration/60 < 5) AS 'total'
+-- FROM Sessions
+-- UNION
+-- SELECT '[5-10>' AS 'bin', SUM(duration/60 >= 5 AND duration/60 < 10) AS 'total'
+-- FROM Sessions
+-- UNION
+-- SELECT '[10-15>' AS 'bin', SUM(duration/60 >= 10 AND duration/60 < 15) AS 'total'
+-- FROM Sessions
+-- UNION
+-- SELECT '15 or more' AS 'bin', SUM(duration/60 >= 15) AS 'total'
+-- FROM Sessions
