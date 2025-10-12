ab59e329866b464fc47203f170d0686ccbf91cc6
seongho <seongho4996@naver.com>
2025-10-12 12:31:53 +0900

    feat: refactor maximumTotalDamage function to use frequency map for improved performance

--- Javascript/review/maximum-total-damage-with-spell-casting.js
+++ Javascript/review/maximum-total-damage-with-spell-casting.js
@@ -2,34 +2,42 @@
  * @param {number[]} power
  * @return {number}
  */
-var maximumTotalDamage = function(power) {
-    power.sort((a, b) => a - b > 0 ? 1: -1);
+var maximumTotalDamage = function (power) {
+  power.sort((a, b) => (a - b > 0 ? 1 : -1));
+  const mp = new Map();
 
-    let dp = Array.from({length: power.length}).fill(-1);
-    
-    const f = (start) => {
-        if(dp[start] !== -1){
-            return dp[start];
-        }
+  for (let i = 0; i < power.length; i++) {
+    const p = power[i];
+    mp.set(p, (mp.get(p) ?? 0) + 1);
+  }
 
-        let answer = power[start];
-        for(let i = start + 1; i < power.length; i++) {
-            const diff = Math.abs(power[start] - power[i]);
+  const arr = [...mp].sort((a, b) => (a[0] - b[0] > 0 ? 1 : -1));
+  console.log(arr);
+  let dp = Array.from({ length: arr.length }).fill(-1);
 
-            if(diff === 0 || diff > 2) {
-                answer = Math.max(answer, power[start] + f(i));
-            }
-        }
-        dp[start] = answer;
-        return answer;
+  const f = (start) => {
+    if (dp[start] !== -1) {
+      return dp[start];
+    }
+    let [p, cnt] = arr[start];
+    let answer = p * cnt;
+    for (let i = start + 1; i < arr.length; i++) {
+      const diff = Math.abs(arr[start][0] - arr[i][0]);
+
+      if (diff === 0 || diff > 2) {
+        answer = Math.max(answer, arr[start][0] * arr[start][1] + f(i));
+      }
     }
 
-    
-    let answer = 0;
-
-    for (let i = 0; i < power.length; i++) {
-        answer = Math.max(answer, f(i));
-    }
-
+    dp[start] = answer;
     return answer;
+  };
+
+  let answer = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    answer = Math.max(answer, f(i));
+  }
+
+  return answer;
 };
